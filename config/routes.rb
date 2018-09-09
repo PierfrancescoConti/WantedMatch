5d725b34365bc002eaecc0e5dbf8b1f0c9d29f8d
Andrea Fioraldi <andreafioraldi@gmail.com>
2018-09-09 11:59:44 +0200

    google login and calendar pretty buttons

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#homepage'
   get '', to: 'home#homepage', as: 'home'
